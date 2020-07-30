6e358e78589f14713e516f0272dab2acb8af33cf
Alex Syhabouth <alex.syhabouth@gmail.com>
2020-07-30 03:52:08 -0700

    fixed edit forms

--- forms.py
+++ forms.py
@@ -18,8 +18,8 @@ class AnimeEditForm(FlaskForm):
     """Form for editing animes."""
 
     title = StringField('Title', validators=[DataRequired()])
-    season = SelectField('Season', choices=[('spring', 'Spring'), ('summer', 'Summer'),
-    ('fall', 'Fall'), ('winter', 'Winter')], validators=[DataRequired()])
+    season = SelectField('Season', choices=[('Spring', 'Spring'), ('Summer', 'Summer'),
+    ('Fall', 'Fall'), ('Winter', 'Winter')], validators=[DataRequired()])
     year = IntegerField('Year', validators=[DataRequired()])
     airing_datetime = DateTimeField('Air Date and Time')
     image = StringField('Image URL', validators=[URL()])
--- static/app.js
+++ static/app.js
@@ -1,18 +1,5 @@
-// async function updateAnimes() {
-//     console.log("click")
-//     const response = await axios.get("https://api.jikan.moe/v3/season");
-//     await axios.post("http://127.0.0.1:5000/refreshanimes", response.data);
-//     location.reload(true);
-// }
-
 $("#refresh-animes").click(async () => {
     const response = await axios.get("https://api.jikan.moe/v3/season");
     const host = await axios.post("https://aniseason.herokuapp.com/refreshanimes", response.data);
     location.reload(true);
 });
-
-
-// setInterval(async () => {
-//     const response = await axios.get("https://api.jikan.moe/v3/season");
-//     if (`${response["season_name"]} ${response["season_year"]}` == )
-// }, 86400000)
